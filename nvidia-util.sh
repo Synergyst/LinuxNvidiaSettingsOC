bcc6a16388bb764b4ae047ef2d43f9fa453026b1
Synergyst <coletonm97@gmail.com>
2022-08-14 02:37:52 -0500

    Update nvidia-util.sh

--- nvidia-util.sh
+++ nvidia-util.sh
@@ -7,7 +7,12 @@ fi
 cd /tmp/nv-oc-util-temp
 
 edid='AP///////wANCRu9AAAAAB4XAQOAWDJ4up2Bo1RMmSYPUFQlTwBxQIEAgUCBgJUAlQ+zAKlAZBkAQEEAJjAYiDYAoFoAAAAeAjqAGHE4LUBYLEUAoFoAAAAeAAAA/QAyPB5EDwIAICAgICAgAAAA/ABWR0EgRElTUExBWQogAa0CAxtyIwkHB4MBAABnAwwAEACAIUMBEITiAA8BHQByUdAeIG4oVQCBSQAAAB4AAAAQAAAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAvg=='
-filename="NVIDIA-Linux-x86_64-510.60.02.run"
+driverversion="510.60.02"
+# SHA256 sum of the driver
+# TODO: Add a commented list of sums for each common driver version
+driversum="a800dfc0549078fd8c6e8e6780efb8eee87872e6055c7f5f386a4768ce07e003"
+driverurl='https://us.download.nvidia.com/XFree86/Linux-x86_64/$driverversion/NVIDIA-Linux-x86_64-$driverversion.run'
+filename="NVIDIA-Linux-x86_64-$driverversion.run"
 
 generate_xorg_conf () {
   if [[ $# -ne 1 ]]; then
@@ -17,14 +22,14 @@ generate_xorg_conf () {
   fi
 
   HEADSECTION=`echo -ne "Section \"ServerFlags\"\n        Option \"BlankTime\" \"0\"\n        Option \"StandbyTime\" \"0\"\n        Option \"SuspendTime\" \"0\"\n        Option \"OffTime\" \"0\"\nEndSection\n\nSection \"ServerLayout\"\n        Identifier     \"Layout0\"\n"`
-  for ((m=0; m &lt;= $1; m++)); do
+  for ((m=0; m <= $1; m++)); do
     HEADSECTION+=`echo -ne "\n        Screen $m \"Screen$m\" 0 0\n"`
   done
   HEADSECTION+=`echo -ne "\n        InputDevice    \"Mouse0\" \"CorePointer\"\nEndSection\n\nSection \"Module\"\n        Disable "glx"\nEndSection
     \nSection \"InputDevice\"\n        Identifier     \"Mouse0\"\n        Driver         \"mouse\"\n        Option         \"Protocol\" \"auto\"\
     \n        Option         \"Device\" \"/dev/psaux\"\n        Option         \"Emulate3Buttons\" \"no\"\n        Option         \"ZAxisMapping\" \"4 5\"\nEndSection
     \nSection \"InputDevice\"\n        Identifier     \"Keyboard0\"\n        Driver         \"kbd\"\nEndSection\n\nSection \"Monitor\"\n        Identifier     \"Monitor0\"\n        Option         \"DPMS\" \"0\"\nEndSection\n\n\n"`
-  for ((m=0; m &lt;= $1; m++)); do
+  for ((m=0; m <= $1; m++)); do
     DEVSCRSECTION+=`echo -ne "\n\nSection \"Device\"\n        Identifier     \"Device$m\"\n        Driver         \"nvidia\"\n        Option         \"Coolbits\" \"31\"\
     \n        BusID          \"PCI:"`
     DEVSCRSECTION+=$(nvidia-xconfig --query-gpu-info|grep -E 'PCI BusID'|cut -d' ' -f6|awk -F'[:]' '{ printf "%d:%d.%d\n", $2, $3, $4 }'|sed -n $(($m+1))p)
@@ -33,37 +38,37 @@ generate_xorg_conf () {
   done
   HEADSECTION+="${DEVSCRSECTION}"
 
-  echo "$edid" | base64 -d &gt; edid.bin
-  echo -ne "needs_root_rights = auto\nallowed_users=anybody\n" &gt; Xwrapper.config
-  echo "${HEADSECTION}" &gt; xorg.conf
+  echo "$edid" | base64 -d > edid.bin
+  echo -ne "needs_root_rights = auto\nallowed_users=anybody\n" > Xwrapper.config
+  echo "${HEADSECTION}" > xorg.conf
 }
 
 help_dialog() {
+  echo -ne '\nNvidia utility\n\nThis tool aims to try to simplify the setup and installation of the Nvidia drivers for new Linux users.\n\nDisclaimer: Please understand that this tool can not guarantee that it will work correctly for all systems.\nThe aim is to try to get the tool to work for most use scenarios.\nPlease report any issues here if you can not find a solution posted already: https://github.com/Synergyst/LinuxNvidiaSettingsOC/issues\n\n'
   echo -ne '\nUsage of command-line options\n\tOptions:'
   echo -ne '\n\t  drivers\t\tVerify driver installation and then ask to install drivers or not\n'
   echo -ne '\n\t  xorg\t\t\tVerify driver installation and then generate Xorg-related files\n'
   echo -ne '\n\t  oc\t\t\tNot yet implemented\n'
-  echo -ne '\n\t  cleantemp\t\tRemoves the temporary storage directory (/tmp/nv-oc-util-temp)\n'
+  echo -ne '\n\t  cleantemp\t\tRemoves the temporary storage directory (Deletes downloaded driver(s) and/or Xorg files in /tmp/nv-oc-util-temp/)\n'
   echo -ne '\n\t  uninstalldrivers\tSimilar to DDU on Windows; tries to perform a clean uninstallation of all Nvidia packages\n\n'
   exit 1
 }
 
 verify_driver_download() {
-  origSum="a800dfc0549078fd8c6e8e6780efb8eee87872e6055c7f5f386a4768ce07e003"
-  echo "$origSum $filename"|sha256sum --check --status
+  echo "$driversum $filename"|sha256sum --check --status
   sumResult=$?
   downloadSum=`sha256sum $filename|awk '{print $1}'`
   if [[ $sumResult == 0 ]]; then
     echo "File verified correctly. SHA256 sum matched $downloadSum"
   else
-    echo "File verified incorrectly. SHA256 sum of downloaded file ($downloadSum) did not match $origSum, exiting.."
+    echo "File verified incorrectly. SHA256 sum of downloaded file ($downloadSum) did not match $driversum, exiting.."
     exit 1
   fi
 }
 
 download_drivers() {
   if [ ! -f "$filename" ]; then
-    wget --output-document=$filename https://us.download.nvidia.com/XFree86/Linux-x86_64/510.60.02/NVIDIA-Linux-x86_64-510.60.02.run
+    wget --output-document=$filename "$driverurl"
     chmod +x $filename
   fi
   verify_driver_download
@@ -88,7 +93,7 @@ verify_drivers () {
       echo "nvidia-smi was found."
       echo
       echo "Checking if nvidia-smi is functioning."
-      nvidia-smi 2&gt;&amp;1 &gt;/dev/null
+      nvidia-smi 2>&1 >/dev/null
       if [[ "$?" -ne "0" ]]; then
         echo "nvidia-smi did not return an exit code of 0, this is likely due to a bad installation of the Nvidia drivers or some mismatch of driver components."
       else
